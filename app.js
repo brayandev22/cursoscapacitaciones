f4f42bd02d4255abf1cbbd9173f4ef5a1ee7dc40
brayandev22 <brayangamarrasusaya@gmail.com>
2024-08-28 16:00:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql'); // Importar el módulo mysql
+const mysql = require('mysql2'); // Importar el módulo mysql
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
